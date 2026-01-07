ed9f9d48ffb95502ac40fb09eb5fd32a391b8bbd
Aman Pal <apal60525@gmail.com>
2026-01-07 23:11:40 +0530

    update multiple files in a single time

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
-const express= require('express')
+import express from "express"
+import colours from "color"
+import dotenv from "dotenv"
+import morgan from "morgan"
+import connectDB from "./config/db.js"
+import authroutes from "./routes/authRoutes.js"
+
+//configure env
+dotenv.config();
+
+
+//database config
+connectDB();
+ 
+ 
 //react object
 const app = express() 
+
+//middleware
+app.use(express.json());
+app.use(morgan('dev'))
+
+//routes
+app.use("/api/v1/auth", authroutes)
 
 //rest api
 app.get('/', (req, res) => {
