8500679a3738780bb659195647d18b5f4b9676d1
Aman Pal <apal60525@gmail.com>
2026-01-07 22:43:21 +0530

    creat userSchema

--- model/usermodels.js
+++ model/usermodels.js
@@ -1,1 +1,40 @@
 import mongoes from mongoes
+
+const userSchema = new mongoes.Schema({ 
+    name:{
+        type:String,
+        required:true,
+        trim: true
+
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+         
+    },
+    password:{
+        type:String,
+        required:true,
+        unique: true
+    },
+    phone:{
+        type:String,
+        required:true,
+         
+    },
+    address:{
+        type:String,
+        required:true,
+    },
+    role:{
+        type:String,
+        default:0
+    }
+
+
+
+
+},{timestamps:true});
+
+export default mongoes.model("User",userSchema);
