0225a5cb5447e3916e7eb15719855ae6b98dcc0b
Aman Pal <apal60525@gmail.com>
2026-01-13 23:03:38 +0530

    login aPI

--- controller/authController.js
+++ controller/authController.js
@@ -1,4 +1,6 @@
 import usermodels  from '../model/usermodels.js'
+import { comparePassword,hashPassword } from './../helper/authHelper.js'
+import JWT from 'jsonwebtoken'
 export const registerController = async(req,res) =>{
     try{
         const {name,email,password}= req.body   
@@ -47,5 +49,58 @@ export const registerController = async(req,res) =>{
             error
         })
 
+    }
+}
+//LOGIN ||POST
+export const loginController = async(req,res) =>{
+    try{
+        const{email,password} = req.body
+        //validation
+        if(!email,password){
+            return res.status(404).send({
+                success:false,
+                message:"Invalid passsword, try again!!"
+
+
+            })
+        }
+        const user = await userModel.findOne({email})
+        if(!user){
+            return res.status(404).send({
+                success:false,
+                message:"Email is not registered"
+            })
+        }  
+        const match = await comparePassword(password, user.password)
+        if(!match){
+        return res.status(200).send({
+            success:false,
+            message:"Invalid password"
+        })
+        //token
+        const token = await JWT.sign({_id:user._id},process.env.JWT_SECRET,{
+            expiresIn:'7d'
+        })  
+        res.status(200).send({
+            success:true,
+            message:"Login successful",
+            user:{
+                name:user.name,
+                email:user.email,
+                phone:user.phone,
+                address:user.address,
+                role:user.role
+            },
+            token
+        })
+    }
+
+    }catch(error){
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message:'Error in Login',
+            error
+        }) 
     }
 }
